0989e1819f6f168a1cf84f668482ad7a16d709a8
Ibrahim Ehab <ibrahim.201649@gmail.com>
2025-05-16 04:01:55 +0300

    16/5

--- terraform/3-main.tf
+++ terraform/3-main.tf
@@ -1,12 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "6.0.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"  
+module "vpc" {
+    source = "./modules/vpc"
+    VPC_CIDR             =  var.VPC_CIDR    
+    cluster_name         =  var.cluster_name
+    private_subnet_cidr  =  var.private_subnet_cidr
+    public_subnet_cidr   =  var.public_subnet_cidr
+    Availability_Zones   =  var.Availability_Zones
 }
--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,9 +1,3 @@
-variable "region" {
-  type = string
-  description = "AWS region"
-  default  = "us-east-1"
-}
-
 variable "VPC_CIDR" {
     description = "Cidr for the VPC"
     type = string
@@ -17,11 +11,9 @@ variable "cluster_name" {
     default = "Feedback-app"
 }
 
-
 variable "private_subnet_cidr" {
     description = "Cidr for the private subnet"
     type = list(string)
-    default = [ "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24" ]
     }
 
 
@@ -29,12 +21,13 @@ variable "private_subnet_cidr" {
 variable "public_subnet_cidr" {
     description = "Cidr for the public subnet"
     type = list(string)
-    default = [ "10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"  ]
 }
 
 
 variable "Availability_Zones" {
     description = "Availability zones"
     type = list(string)
-    default = [ "us-east-1a","us-east-1b","us-east-1c" ]
 }
+
+
+
